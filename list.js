0d1f551afc68a416291df51692f763ff07961454
Duane McDonald <mcdonald.duane13@gmail.com>
2021-02-19 10:47:11 -0500

    Basic JS is working
    styling & todolist JS set. needs check&delete functions

--- list.js
+++ list.js
@@ -1,21 +1,32 @@
-// Create a new list item when clicking on the "Add Item" button
+// creates element of the to-do item
 function newItem() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
     var newItemName = document.createTextNode(inputValue);
-    li.appendChild(newItemName);
+    var dueBy = document.createElement("span")
+    dueBy.innerText = "Due By: "
+    dueBy.className = "dueBy"
+    var dateValue = document.getElementById("myDate").value;
+    var dueDate = document.createElement("input");
+    dueDate.type = "date";
+    dueDate.className = "dueDate";
+    dueDate.defaultValue = dateValue;
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    var closeBtn = document.createElement("button");
+    var fire = document.createTextNode(" \uD83D\uDD25");
+    closeBtn.className = "close";
+    closeBtn.appendChild(fire);
+// Creates line item with all the pieces, if not blank 
+    li.append(checkbox,"   ",newItemName," ",dueBy,dueDate,"  ",closeBtn);
     if (inputValue === '') {
       alert("Enter a task");
     } else {
       document.getElementById("items").appendChild(li);
     }
+//reset inputs
     document.getElementById("myInput").value = "";
-  
-    var closeBtn = document.createElement("button");
-    var fire = document.createTextNode(" \uD83D\uDD25");
-    closeBtn.className = "close";
-    closeBtn.appendChild(fire);
-    li.appendChild(closeBtn);
+    document.getElementById("myDate").value = "";
   
     for (i = 0; i < close.length; i++) {
       close[i].onclick = function() {
@@ -24,5 +35,5 @@ function newItem() {
       }
     }
   }
-      // var dateValue = document.getElementById("do-by").value;
-    // var itemDate = document.createTextNode(inputValue);
+
+// fire button functionality
--- listener.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector('input');
-const log = document.getElementById('theForm');
-
-input.addEventListener('input', updateList);
-
-function updateList(e) {
-  log.textContent += e.target.value;
-}
